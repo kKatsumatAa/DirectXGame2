5dc682ba89044a9b95efafa4bad9cb51ce90eb4e
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-04-12 19:57:04 +0900

    02_01_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,10 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	/*delete sprite_;*/
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +17,69 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("kasuga.png");
+	//スプライト初期化
+	/*sprite_ = Sprite::Create(textureHandle_, {100, 50});*/
+	//モデル初期化
+	model_ = Model::Create();
+	// x,y,z方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	// x,y,z軸回りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	// x,y,zの位置を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	//ワールドトランスフォーム初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクション初期化
+	viewProjection_.Initialize();
+	////サウンドデータの読み込み
+	// soundDataHandle_ = audio_->LoadWave("fanfare.wav");
+	////サウンド再生
+	// audio_->PlayWave(soundDataHandle_,true);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	////スプライトの今の座標を取得
+	// XMFLOAT2 position = sprite_->GetPosition();
+	////座標を{2,0}移動
+	// position = {position.x + 2.0f, position.y + 1.0f};
+	// sprite_->SetPosition(position);
+
+	////スペースキーを押した瞬間
+	// if (input_->TriggerKey(DIK_SPACE))
+	//{
+	//	//音声停止
+	//	audio_->StopWave(soundDataHandle_);
+	// }
+
+	//デバックテキストの表示
+	/*debugText_->Print("kaizokuou ni oreha naru.", 50, 50, 1.0f);*/
+
+	//書式指定付き表示
+	/*debugText_->SetPos(50, 70);
+	debugText_->Printf("year%d", 2001);*/
+
+	////変数の値をインクリメント
+	// value_++;
+	////値を含んだ文字列
+	// std::string strDebug = std::string("Value:") + std::to_string(value_);
+	////デバッグテキストの表示
+	// debugText_->Print(strDebug, 50, 50, 1.0f);
+
+	// x,y,z変換表示
+	debugText_->SetPos(50, 0);
+	debugText_->Printf(
+	  "scale %f,%f,%f\n", worldTransform_.scale_.x, worldTransform_.scale_.y,
+	  worldTransform_.scale_.z);
+	debugText_->SetPos(50, 50);
+	debugText_->Printf(
+	  "rotation %f,%f,%f", worldTransform_.rotation_.x, worldTransform_.rotation_.y,
+	  worldTransform_.rotation_.z);
+	debugText_->SetPos(50, 100);
+	debugText_->Printf(
+	  "translation %f,%f,%f", worldTransform_.translation_.x, worldTransform_.translation_.y,
+	  worldTransform_.translation_.z);
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +107,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -56,6 +120,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	/*sprite_->Draw();*/
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DirectXCommon.h"
 #include "DebugText.h"
+#include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
@@ -47,6 +47,22 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	////スプライト
+	// Sprite* sprite_ = nullptr;
+	// 3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	////サウンドデータハンドル
+	// uint32_t soundDataHandle_ = 0;
+
+	////値を表示したい変数
+	// int32_t value_ = 0;
 
 	/// <summary>
 	/// ゲームシーン用
